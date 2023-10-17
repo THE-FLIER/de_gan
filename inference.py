157fc895e3efa44615dcf00abcee3c7a326337fa
THE-FLIER <2397252522@qq.com>
2023-10-17 10:34:48 +0800

    infer update

--- inference.py
+++ inference.py
@@ -11,7 +11,11 @@ from utils import *
 from models.models import *
 import cv2
 from skimage.metrics import peak_signal_noise_ratio as ski_psnr
-
+#gpu
+gpus = tf.config.experimental.list_physical_devices('GPU')
+tf.config.set_visible_devices(gpus[0], 'GPU')  # 使用or禁用用所有GPU设备
+#cpu
+# tf.config.set_visible_devices(tf.config.list_physical_devices('CPU'), 'CPU')  # 启用CPU设备
 def infer(generator,image,out_put):
     path = os.listdir(image)
     for i in tqdm(range(len(path))):
@@ -22,28 +26,77 @@ def infer(generator,image,out_put):
             test_image.save('curr_infer_image.png')
             test_image = plt.imread('curr_infer_image.png')
 
-            test_image = np.asarray(test_image)
-            h, w = test_image.shape
-            top_left = np.resize(test_image[:h // 2, :w // 2], (256,256))
-            top_right = np.resize(test_image[:h//2, w//2:], (256,256))
-            bottom_left = np.resize(test_image[h//2:, :w//2], (256,256))
-            bottom_right = np.resize(test_image[h//2:, w//2:], (256,256))
+            clean_image = np.asarray(test_image)
 
-            # 预测
-            t_l = predict(top_left, generator)
-            t_r = predict(top_right, generator)
-            b_l = predict(bottom_left, generator)
-            b_r = predict(bottom_right, generator)
+            h, w = clean_image.shape
 
-        # 拼接预测结果
-            result = np.concatenate([np.concatenate([t_l, t_r], axis=1),
-                                     np.concatenate([b_l, b_r], axis=1)],
-                                    axis=0)
-            imageio.imwrite(f'{out_put}' + name, result)
+            if w > h:
+                crop_w = w // 4
+
+                left1 = 0
+                right1 = crop_w
+
+                left2 = crop_w
+                right2 = crop_w * 2
+
+                left3 = crop_w * 2
+                right3 = crop_w * 3
+
+                left4 = crop_w * 3
+                right4 = w
+
+                # 从左往右裁剪
+                img1 = im(clean_image[:, left1:right1])
+                img2 = im(clean_image[:, left2:right2])
+                img3 = im(clean_image[:, left3:right3])
+                img4 = im(clean_image[:, left4:right4])
+
+                img1 = predict(img1, generator)
+                img2 = predict(img2, generator)
+                img3 = predict(img3, generator)
+                img4 = predict(img4, generator)
+                # 拼接预测结果
+                result = np.concatenate([np.concatenate([img1, img2], axis=1),
+                                         np.concatenate([img3, img4], axis=1)],
+                                        axis=1)
+                result_img = Image.fromarray(result)
+                result_img = result_img.resize((w, h))
+                result = np.asarray(result_img)
+                imageio.imwrite(f'{out_put}' + name, result)
+            else:
+                crop_h = h // 4
+                top1 = 0
+                bottom1 = crop_h
+                top2 = crop_h
+                bottom2 = crop_h * 2
+                top3 = crop_h * 2
+                bottom3 = crop_h * 3
+                top4 = crop_h * 3
+                bottom4 = h
+
+                # 裁剪成4份
+                img1 = im(clean_image[top1:bottom1, :])
+                img2 = im(clean_image[top2:bottom2, :])
+                img3 = im(clean_image[top3:bottom3, :])
+                img4 = im(clean_image[top4:bottom4, :])
+
+                img1 = predict(img1, generator)
+                img2 = predict(img2, generator)
+                img3 = predict(img3, generator)
+                img4 = predict(img4, generator)
+            # 拼接预测结果
+                result = np.concatenate([np.concatenate([img1, img2], axis=0),
+                                         np.concatenate([img3, img4], axis=0)],
+                                        axis=0)
+                result_img = Image.fromarray(result)
+                result_img = result_img.resize((w, h))
+                result = np.asarray(result_img)
+                imageio.imwrite(f'{out_put}' + name, result)
 
 if __name__ == "__main__":
     generator = generator_model(biggest_layer=1024)
     generator.load_weights("weights/binarization_generator_weights.h5")
-    image = 'CLEAN/VALIDATION/DATA1_1/'
-    out_put = ''
-    predict(generator,image,out_put)
+    image = 'example/'
+    out_put = 'outputs/example/'
+    os.makedirs(out_put, exist_ok=True)
+    infer(generator, image, out_put)
